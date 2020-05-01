80deb8a6b6d66278b8c0f9884da0d9635502a9fb
Edy kurniawan <edy.kurniawan280898@gmail.com>
2020-05-01 14:32:21 +0700

    Done upload img barang
    selesai crud data barang + img

--- application/controllers/Admin/Barang.php
+++ application/controllers/Admin/Barang.php
@@ -70,78 +70,13 @@ class Barang extends CI_Controller {
 
     public function ajax_delete($id)
     {
+        $barang= $this->M_barang->edit($id);
+        if(file_exists('./assets/upload/barang/'.$barang->Img) && $barang->Img)
+        unlink('./assets/upload/barang/'.$barang->Img);
         $this->M_barang->delete_by_kode($id);
         echo json_encode(array("status" => TRUE));
     }
-
-    function ajax_add(){
-
-        date_default_timezone_set('Asia/Jakarta'); # add your city to set local time zone
-        $now = date('Y-m-d H:i:s');
-
-        $this->form_validation->set_rules('kode','kode','trim|required');
-        $this->form_validation->set_rules('nama','nama','trim|required');
-        $this->form_validation->set_rules('toko','toko','trim');
-        $this->form_validation->set_rules('stok','stok','trim|required');
-        $this->form_validation->set_rules('status','status','trim|required');
-        $this->form_validation->set_rules('satuan','satuan','trim|required');
-        $this->form_validation->set_rules('kat','kat','trim|required');
-
-        if($this->form_validation->run() == false)
-        {
-            
-        }
-        else
-        {
-
-        //upload photo
-        $config['upload_path'] = './assets/upload/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size']     = '100';
-        $config['max_width'] = '1024';
-        $config['max_height'] = '768';
-
-        $this->load->library('upload', $config);
-
-        if ( ! $this->upload->do_upload('img'))
-                {
-                    $data_image="default.jpg";
-                }
-                else
-                {
-                    $data_image=$this->upload->data('file_name');
-                }
-
-        $kode = $this->security->sanitize_filename($this->input->post('kode'));
-        $reftoko = $this->security->sanitize_filename($this->input->post('toko'));
-        $nama = $this->security->sanitize_filename($this->input->post('nama'));
-        $harga = $this->security->sanitize_filename($this->input->post('harga'));
-        $stok = $this->security->sanitize_filename($this->input->post('stok'));
-        $refkat = $this->security->sanitize_filename($this->input->post('kat'));
-        $status = $this->security->sanitize_filename($this->input->post('status'));
-        $satuan = $this->security->sanitize_filename($this->input->post('satuan'));
-        $ket = $this->security->sanitize_filename($this->input->post('ket'));
- 
-        $data = array(
-            "Kode"       => $kode,
-            "Nama"       => $nama,
-            "Reftoko"    => $reftoko,
-            "Harga"      => $harga,
-            "Stok"       => $stok,
-            "Refkategori"    => $refkat,
-            "Status"      => $status,
-            "Satuan"      => $satuan,
-            "Ket"        => $ket,
-            "Datei"      => $now,
-            "Img"        => $data_image
-                    );
-            
-        $this->M_barang->inputdata($data,'barang');
-        echo json_encode(array("status" => TRUE));
-        }
-
-    }
-    
+   
        public function ajax_edit($id)
     {
         $data = $this->M_barang->edit($id);
@@ -194,11 +129,88 @@ class Barang extends CI_Controller {
             $where = array(
             'ID' => $id
     );
+
+        //Update img
+        if($this->input->post('remove_photo')) // if remove photo checked
+        {
+            if(file_exists('./assets/upload/barang/'.$this->input->post('remove_photo')) && $this->input->post('remove_photo'))
+                unlink('./assets/upload/barang/'.$this->input->post('remove_photo'));
+            $data['Img'] = '';
+        }
+ 
+        if(!empty($_FILES['photo']['name']))
+        {
+            $upload = $this->_do_upload();
+             
+            //delete file
+            $barang= $this->M_barang->edit($id);
+            if(file_exists('./assets/upload/barang/'.$barang->Img) && $barang->Img)
+            unlink('./assets/upload/barang/'.$barang->Img);
+ 
+            $data['Img'] = $upload;
+        } 
  
         $this->M_barang->update($where,$data);
         echo json_encode(array("status" => TRUE));
     }
 
 }
+
+    public function ajax_add()
+    { 
+        $kode = $this->security->sanitize_filename($this->input->post('kode'));
+        $reftoko = $this->security->sanitize_filename($this->input->post('toko'));
+        $nama = $this->security->sanitize_filename($this->input->post('nama'));
+        $harga = $this->security->sanitize_filename($this->input->post('harga'));
+        $stok = $this->security->sanitize_filename($this->input->post('stok'));
+        $refkat = $this->security->sanitize_filename($this->input->post('kat'));
+        $status = $this->security->sanitize_filename($this->input->post('status'));
+        $satuan = $this->security->sanitize_filename($this->input->post('satuan'));
+        $ket = $this->security->sanitize_filename($this->input->post('ket'));
+ 
+        $data = array(
+            'Kode'       => $kode,
+            'Nama'       => $nama,
+            'Reftoko'    => $reftoko,
+            'Harga'      => $harga,
+            'Stok'       => $stok,
+            'Refkategori'    => $refkat,
+            'Status'      => $status,
+            'Satuan'      => $satuan,
+            'Ket'        => $ket
+                    );
+ 
+         if(!empty($_FILES['photo']['name']))
+            {
+                $upload = $this->_do_upload();
+                $data['Img'] = $upload;
+            }
+ 
+        $this->M_barang->inputdata($data,'barang');
+        echo json_encode(array("status" => TRUE));
+    }
+
+    private function _do_upload()
+    {
+        $config['upload_path']          = './assets/upload/barang/';
+        $config['allowed_types']        = 'gif|jpg|png';
+        $config['max_size']             = 100; //set max size allowed in Kilobyte
+        $config['max_width']            = 1000; // set max width image allowed
+        $config['max_height']           = 1000; // set max height allowed
+        $config['file_name']            = round(microtime(true) * 1000); //just milisecond timestamp fot unique name
+ 
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+ 
+        if(!$this->upload->do_upload('photo')) //upload and validate
+        {
+            $data['inputerror'][] = 'photo';
+            $data['error_string'][] = 'Upload error: '.$this->upload->display_errors('',''); //show ajax error
+            $data['status'] = FALSE;
+            echo json_encode($data);
+            exit();
+        }
+        return $this->upload->data('file_name');
+    }
     
 }
